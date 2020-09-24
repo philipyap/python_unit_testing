cbef40a7bdaddde4c8ee66e06177f061ab72e2b8
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-24 15:16:35 -0700

    [test_employee]edd 3 test for employee class and pass test

--- employee.py
+++ employee.py
@@ -6,21 +6,16 @@ class Employee:
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.pay = pay
+        self.pay = int(pay)
 
     @property
     def email(self):
-        return '{}.{}@sei713.com'.format(self.first, self.last)
+        return '{}.{}@sei713.com'.format(self.first, self.last).lower()
 
     @property 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
-
-rome = Employee('Rome', 'Bell', '1000000')
-
-print(rome.email)
-print(rome.fullname)
+        self.pay = round(float(self.pay * self.raise_amount))
     
--- test_employee.py
+++ test_employee.py
@@ -1,2 +1,42 @@
 import unittest
-import employee
+from employee import Employee
+
+class TestEmployee(unittest.TestCase):
+    """Test for Employee class"""
+
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    def setUp(self):
+        print('setUp')
+        self.levin = Employee('Levin', 'Batallones', 2000000)
+        self.martin = Employee('Martin', 'Briceno', 1999999)
+        self.rome = Employee('Rome', 'Bell', 1000000)
+        self.adam = Employee('Adam', 'Honroe', 50000)
+    
+    def tearDown(self):
+        print('tearDown\n')
+    
+    def test_email(self):
+        self.assertEqual(self.levin.email, 'levin.batallones@sei713.com')
+        self.assertEqual(self.martin.email, 'martin.briceno@sei713.com')
+        self.assertEqual(self.rome.email, 'rome.bell@sei713.com')
+        self.assertEqual(self.adam.email, 'adam.honroe@sei713.com')
+
+    def test_fullname(self):
+        
+        self.assertEqual(self.rome.fullname, 'Rome Bell')
+        self.assertEqual(self.adam.fullname, 'Adam Honroe')
+
+    def test_raise_amount(self):
+
+        self.rome.apply_raise()
+        self.adam.apply_raise()
+
+        self.assertEqual(self.rome.pay, 1150000)
+        self.assertEqual(self.adam.pay, 57500)
+
+if __name__ == '__main__':
+    unittest.main()
+
